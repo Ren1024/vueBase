9c418f751bced7dd4078fe43b7ff4d5200959bdd
chumen <582831743@qq.com>
2022-07-08 18:00:35 +0800

    'todos_care_two_init'

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
-    <div>
-        
+<div class="todo-container">
+    <div class="todo-wrap">
+        <HeaderVue></HeaderVue>
+        <ListVue></ListVue>
+        <FooterVue></FooterVue>
+
     </div>
+  </div>
 </template>
 
 <script>
+import HeaderVue from './components/Header.vue'
+import ListVue from './components/List.vue'
+import FooterVue from './components/Footer.vue'
+
 export default {
     name:'',
+    components:{
+        HeaderVue,
+        ListVue,
+        FooterVue
+    }
 }
 </script>
 
 <style>
-
+/*app*/
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,35 @@
+<template>
+    <ul class="todo-main">
+        <ItemVue></ItemVue>
+    </ul>
+</template>
+
+<script>
+import ItemVue from './Item.vue'
+
+export default {
+    name:'',
+    components:{
+        ItemVue,
+    }
+}
+</script>
+
+<style>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
